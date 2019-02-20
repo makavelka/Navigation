f41e1c48fb0bb757f4b6fd5ba4161f307d882c9b
Vladimir <vovchilnik@yandex.ru>
2019-02-20 17:45:53 +0300

    commit

--- app/src/main/java/ru/tebloev/navigation/Fragment1.java
+++ app/src/main/java/ru/tebloev/navigation/Fragment1.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -20,39 +22,44 @@ public class Fragment1 extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_fragment1, container, false);
         Button button = v.findViewById(R.id.button);
+        setHasOptionsMenu(true);
 
 
-
-
-        Bundle bundle = new Bundle();
-        bundle.putString("arg1", "bla bla");
-        bundle.putString("arg2", "la la");
-        NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
-        navController.navigate(R.id.action_fragment1_to_fragment2, bundle);
-
-
-        Fragment1Directions.ActionFragment1ToFragment2 action =
-                Fragment1Directions.actionFragment1ToFragment2(true);
-        action.setArg1("bla bla")
-                .setArg2(33333L);
-        navController.navigate(action);
-
-
-        NavOptions.Builder builder = new NavOptions.Builder();
-        builder.setEnterAnim(R.anim.nav_default_enter_anim)
-                .setExitAnim(R.anim.nav_default_exit_anim)
-                .setPopEnterAnim(R.anim.nav_default_pop_enter_anim)
-                .setPopExitAnim(R.anim.nav_default_pop_exit_anim)
-                .setLaunchSingleTop(true)
-                .setPopUpTo(R.id.fragment4, true);
-        navController.navigate(action, builder.build());
-
-
-
-        button.setOnClickListener(v1 -> navController.navigate(R.id.action_fragment1_to_fragment2));
+//
+//        Bundle bundle = new Bundle();
+//        bundle.putString("arg1", "bla bla");
+//        bundle.putString("arg2", "la la");
+//        NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+//        navController.navigate(R.id.action_fragment1_to_fragment2, bundle);
+//
+//
+//        Fragment1Directions.ActionFragment1ToFragment2 action =
+//                Fragment1Directions.actionFragment1ToFragment2(true);
+//        action.setArg1("bla bla")
+//                .setArg2(33333L);
+//        navController.navigate(action);
+//
+//
+//        NavOptions.Builder builder = new NavOptions.Builder();
+//        builder.setEnterAnim(R.anim.nav_default_enter_anim)
+//                .setExitAnim(R.anim.nav_default_exit_anim)
+//                .setPopEnterAnim(R.anim.nav_default_pop_enter_anim)
+//                .setPopExitAnim(R.anim.nav_default_pop_exit_anim)
+//                .setLaunchSingleTop(true)
+//                .setPopUpTo(R.id.fragment4, true);
+//        navController.navigate(action, builder.build());
+//
+//
+//
+//        button.setOnClickListener(v1 -> navController.navigate(R.id.action_fragment1_to_fragment2));
 
 
         return v;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 }
